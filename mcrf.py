843ef9ac831b132d4cc0f0dfded1614b26c57e39
user0 <samneufeld@protonmail.com>
2018-06-27 22:52:26 -0500

    Fixed dependency checks

--- mcrf.py
+++ mcrf.py
@@ -10,7 +10,9 @@ try:
 except:
     pass
 
+# Global vars
 working = True
+macchanger, ip, iwconfig = '', '', ''
 
 def logoPrint():
     print("""
@@ -22,6 +24,7 @@ def logoPrint():
     """)
 
 def changeMac(interface, mode='none'):
+    global ip, iwconfig, macchanger
     if mode == 'none':
         print("")
         print("\033[91m### \033[92m INTERFACE {} \033[91m###\033[0m".format(interface['name']))
@@ -39,39 +42,36 @@ def changeMac(interface, mode='none'):
         else:
             print("\033[91m[*] \033[0mInvalid selection!")
             return()
-    os.system("ip link set {} down".format(interface['name']))
+    os.system("{} link set {} down".format(ip, interface['name']))
     if mode in ['mon', 'all']:
         try:
-            print("")
             if interface['mon'] != 'Wired':
                 if interface['mon'] == 'Managed':
-                    os.system('iwconfig {} mode monitor'.format(interface['name']))
-                    print("\033[91m[*] \033[0mPut station interface \033[92m{}\033[0m in monitor mode.".format(interface['name']))
+                    os.system('{} {} mode monitor'.format(iwconfig, interface['name']))
+                    print("\n\033[91m[*] \033[0mPut station interface \033[92m{}\033[0m in monitor mode.".format(interface['name']))
                     interface['mon'] = 'Monitor'
                 elif interface['mon'] == 'Monitor':
-                    os.system('iwconfig {} mode managed'.format(interface['name']))
-                    print("\033[91m[*] \033[0mPut monitor interface \033[92m{}\033[0m in station mode.".format(interface['name']))
+                    os.system('{} {} mode managed'.format(iwconfig, interface['name']))
+                    print("\n\033[91m[*] \033[0mPut monitor interface \033[92m{}\033[0m in station mode.".format(interface['name']))
                     interface['mon'] = 'Managed'
-                print("")
             else:
-                print("\033[91m[*] \033[0mCannot put a wired interface into monitor mode!")
+                print("\n\033[91m[*] \033[0mCannot put a wired interface into monitor mode!")
         except:
-            print("\033[91m[*] \033[0mInterface cannot be put into monitor mode.")
+            print("\n\033[91m[*] \033[0mInterface cannot be put into monitor mode.")
     if mode in ['mac', 'all']:
         try:
-            os.system("ip link set {} down".format(interface['name']))
-            print("\n\033[91m##### \033[0mINTERFACE \033[92m{} \033[91m#####\033[0m\n".format(interface['name']))
-            os.system('macchanger -A {} 2>/dev/null'.format(interface['name']))
+            os.system("{} link set {} down".format(ip, interface['name']))
+            print("\n\033[91m##### \033[0mINTERFACE \033[92m{} \033[91m#####\033[0m".format(interface['name']))
+            os.system('{} -A {} 2>/dev/null'.format(macchanger, interface['name']))
             time.sleep(0.25)
-            os.system("ip link set {} up".format(interface['name']))
-            print("")
+            os.system("{} link set {} up".format(ip, interface['name']))
         except:
-            print("\033[91m[*] \033[0mInvalid interface!")
+            print("\n\033[91m[*] \033[0mInvalid interface!")
 
 def intGet():
-    global working
+    global working, macchanger, ip ,iwconfig
     interfaces = []
-    a = str(subprocess.check_output('ip link show', shell=True))
+    a = str(subprocess.check_output("{} link show".format(ip), shell=True))
     ints = a.split(': ')
     for i in range(len(ints)):
         if len(ints[i].split()) == 1:
@@ -81,13 +81,13 @@ def intGet():
     # Get interface properties
     for interface in interfaces:
         name = interface['name']
-        macs = str(subprocess.check_output('macchanger -s {}'.format(name), shell=True))
+        macs = str(subprocess.check_output("{} -s {}".format(macchanger, name), shell=True))
         interface['cMac'] = macs.split()[2]
         interface['cVend'] = macs.split("(")[1].split(")")[0]
         interface['pMac'] = macs.split("\n")[1].split()[2]
         interface['pVend'] = macs.split("\n")[1].split("(")[1].split(")")[0]
         try:
-            mon = str(subprocess.check_output('iwconfig {} 2> /dev/null'.format(name), shell=True)).split()
+            mon = str(subprocess.check_output("{} {} 2> /dev/null".format(iwconfig, name), shell=True)).split()
             mon1 = mon[3].split(':')[1]
             if mon1 == 'off/any':
                 mon1 = mon[4].split(':')[1]
@@ -96,6 +96,7 @@ def intGet():
             interface['mon'] = 'Wired'
     intRef = {}
     i = 0
+    print("")
     for item in interfaces:
         print("\033[91m[\033[92m{}\033[91m]\033[0m: {} \033[92m::: \033[0m{} \033[92m// \033[0m{} \033[92m// \033[0m{} ".format(i, item['name'], item['pVend'], item['cVend'], item['mon']))
         intRef[i] = item
@@ -130,13 +131,12 @@ def intGet():
             allInt = True
     if not allInt:
         changeMac(interface)
-macchanger, ip, iwconfig = '', '', ''
 if os.getuid() != 0:
     print("Must be run as root!")
 else:
-    macchanger = subprocess.check_output("which macchanger", shell=True)
-    ip = subprocess.check_output("which ip", shell=True)
-    iwconfig = subprocess.check_output("which iwconfig", shell=True)
+    macchanger = subprocess.check_output("which macchanger", shell=True)[:-1]
+    ip = subprocess.check_output("which ip", shell=True)[:-1]
+    iwconfig = subprocess.check_output("which iwconfig", shell=True)[:-1]
     if macchanger == '':
         print("MACCHANGER not installed! Please install and try again.")
         quit()
@@ -146,7 +146,7 @@ else:
     if iwconfig == '':
         print("IWCONFIG not installed! Please install and try again.")
         quit()
-
+    print(ip)
     logoPrint()
     while working:
         intGet()
